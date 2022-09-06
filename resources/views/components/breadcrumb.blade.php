0faa456dcb6c52734e996d75f6b8c1661d86b681
Sabrina Moura Gandra <smouragandra@gmail.com>
2022-09-05 22:33:36 -0300

    alteração

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,7 +1,9 @@
 <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-          @for($i = 0; $i < sizeof(link)-1; $i++)
-          <li class="breacrumb-item"><a href="#">Home</a></li>
+          @for($i = 0; $i < sizeof($links)-1; $i++)
+          <li class="breacrumb-item">
+            <a href="{{$links[$i]['valor']}}">{{$links[sizeof($links)-1]['nome']}}</a>
+        </li>
         @endfor
         <li class="breacrumb-item active">{{$links[sizeof($links)-1]['nome']}}</li>
         </ol>
--- resources/views/site/vendas.blade.php
+++ resources/views/site/vendas.blade.php
@@ -8,10 +8,10 @@
 <div class="container">
     @component('components.title', ['title' => "Relatorio de vendas - $mes/$ano"])
         @include('components.breadcrumb', ['links' => [
-           ['nome' => 'Home', 'valor' => "http://site/secao/pagina.php",
-           ['nome' => 'Vendas', 'valor' => "http://site/secao/pagina.php",
-           ['nome' => 'Eletrônico', 'valor' => "http://site/secao/pagina.php",
-           ['nome' => 'Informática', 'valor' => "http://site/secao/pagina.php",     
+           ['nome' => 'Home', 'valor' => "http://site/secao/pagina.php"],
+           ['nome' => 'Vendas', 'valor' => "http://site/secao/pagina.php"],
+           ['nome' => 'Eletrônico', 'valor' => "http://site/secao/pagina.php"],
+           ['nome' => 'Informática', 'valor' => "http://site/secao/pagina.php"],     
         ]])
     @endcomponent
     @include('components.table')
